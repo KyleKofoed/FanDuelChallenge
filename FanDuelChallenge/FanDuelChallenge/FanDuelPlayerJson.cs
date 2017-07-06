edd48093e697c69b0e6bb07842e1c88e0f944cd6
kkofoed <kkofoed@mbfinancial.com>
2017-07-05 19:28:39 -0500

    Setup Models for testing

---
+++ FanDuelChallenge/FanDuelChallenge/FanDuelPlayerJson.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FanDuelChallenge
+{
+    public class FanDuelPlayerJson
+    {
+        public FanDuelPlayerJson()
+        { }
+        List<Player> Players { get; set;}
+        
+
+
+    }
+}
+
--- FanDuelChallenge/FanDuelChallenge/Player.cs
+++ FanDuelChallenge/FanDuelChallenge/Player.cs
@@ -1,15 +1,24 @@
-﻿using System;
+﻿
+using System.Collections.Generic;
 
 namespace FanDuelChallenge
 {
     public class Player
     {
-        public Player()
-        { }
-
         public string FirstName { get; set; }
-    
+        public string LastName { get; set; }
+        public Images Images{ get; set;}
+    }
 
+    public class Images
+    {
+        public Image Default;
+    }
+
+    public class Image
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public string URL { get; set; }
     }
 }
-
