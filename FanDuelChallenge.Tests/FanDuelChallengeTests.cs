8150527a5fe63354d5c5a7f88932501b58e9c17c
kkofoed <kkofoed@mbfinancial.com>
2017-07-05 19:15:33 -0500

    added first test

--- FanDuelChallenge.Tests/FanDuelChallengeTests.cs
+++ FanDuelChallenge.Tests/FanDuelChallengeTests.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace FanDuelChallenge.Tests
 {
     public class FanDuelChallengeTests
     {
+        [Fact]
+        public void MyFirstTestMethod()
+        {
+            Xunit.Assert.True(true);
+        }
     }
 }
---
+++ FanDuelChallenge/FanDuelChallenge/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FanDuelChallenge
+{
+    public class Player
+    {
+        public Player()
+        { }
+
+        public string FirstName { get; set; }
+    
+
+    }
+}
+
